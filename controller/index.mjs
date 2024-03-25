07d00002fa71c78f9a0345c449c761f17ec5db8f
Tomaz Lanza <lanza.tomaz@gmail.com>
2024-03-25 18:56:03 -0300

    First commit

---
+++ controller/index.mjs
@@ -0,0 +1,203 @@
+import http from "node:http"
+import { readFileSync } from "fs"
+import { URL } from "node:url"
+
+////////////////////////////////////////////////////////////
+/////setting paths and reading files to be served
+////////////////////////////////////////////////////////////
+
+//////////////////main files
+const indexHTMLPath = new URL("../index.html", import.meta.url)
+const mainScriptPath = new URL("../main.js", import.meta.url)
+const cssFilePath = new URL("../style.css", import.meta.url)
+
+const indexHTML =  readFileSync(indexHTMLPath, {encoding: "utf-8"})
+const mainScript =  readFileSync(mainScriptPath, {encoding: "utf-8"})
+const cssFile =  readFileSync(cssFilePath, {encoding: "utf-8"})
+
+// //////////////////images
+// ///////header background
+const headerBackgroundPath = new URL("../public/images/header-background.png", import.meta.url)
+
+const headerBackground = readFileSync(headerBackgroundPath)
+
+// ///////welcome photos
+const welcomePhoto1Path = new URL("../public/images/first-profile-photo.jpg", import.meta.url)
+const welcomePhoto2Path = new URL("../public/images/second-profile-photo.jpg", import.meta.url)
+
+const welcomePhoto1 = readFileSync(welcomePhoto1Path)
+const welcomePhoto2 = readFileSync(welcomePhoto2Path)
+
+// ////////up arrow
+const upArrowPath = new URL("../public/images/svg/stylized-svg/arrow-up.svg", import.meta.url)
+
+const upArrow = readFileSync(upArrowPath)
+
+// ///////skill section svg
+const webDevSVGPath = new URL("../public/images/svg/stylized-svg/web-programming.svg", import.meta.url)
+const mathAndStatsSVGPath = new URL("../public/images/svg/stylized-svg/statistics.svg", import.meta.url)
+const dataAnalysisSVGPath = new URL("../public/images/svg/stylized-svg/scatterplot.svg", import.meta.url)
+const automationSVGPath = new URL("../public/images/svg/stylized-svg/robot.svg", import.meta.url)
+
+const webDevSVG = readFileSync(webDevSVGPath)
+const mathAndStatsSVG = readFileSync(mathAndStatsSVGPath)
+const dataAnalysisSVG = readFileSync(dataAnalysisSVGPath)
+const automationSVG = readFileSync(automationSVGPath)
+
+// ///////portfolio arrows
+const leftArrowPath = new URL("../public/images/svg/stylized-svg/arrow-left.svg", import.meta.url)
+const rightArrowPath = new URL("../public/images/svg/stylized-svg/arrow-right.svg", import.meta.url)
+
+const leftArrow = readFileSync(leftArrowPath)
+const rightArrow = readFileSync(rightArrowPath)
+
+//////////projects
+const calculatorProjectBackgroundPath = new URL("../public/images/calculator-background.jpg", import.meta.url)
+const rgbFilterProjectBackgroundPath = new URL("../public/images/rgb-filter-background.jpg", import.meta.url)
+
+const calculatorProjectBackground = readFileSync(calculatorProjectBackgroundPath)
+const rgbFilterProjectBackground = readFileSync(rgbFilterProjectBackgroundPath)
+// ///////svg layers
+const layer1Path = new URL("../public/images/svg/layers/layer1.svg", import.meta.url)
+const layer2Path = new URL("../public/images/svg/layers/layer2.svg", import.meta.url)
+
+const layer1 = readFileSync(layer1Path)
+const layer2 = readFileSync(layer2Path)
+
+// ///////social media svg
+const gitHubSVGPath = new URL("../public/images/svg/social-media-logos/Golden-SVG-logos/1-golden-github-logo.svg", import.meta.url)
+const linkedInSVGPath = new URL("../public/images/svg/social-media-logos/Golden-SVG-logos/2-golden-linkedin-logo.svg", import.meta.url)
+const instagramSVGPath = new URL("../public/images/svg/social-media-logos/Golden-SVG-logos/3-golden-instagram-logo.svg", import.meta.url)
+const emailSVGPath = new URL("../public/images/svg/social-media-logos/Golden-SVG-logos/4-golden-email-logo.svg", import.meta.url)
+const whatsappSVGPath = new URL("../public/images/svg/social-media-logos/Golden-SVG-logos/5-golden-whatsapp-logo.svg", import.meta.url)
+
+const gitHubSVG = readFileSync(gitHubSVGPath)
+const linkedInSVG = readFileSync(linkedInSVGPath)
+const instagramSVG = readFileSync(instagramSVGPath)
+const emailSVG = readFileSync(emailSVGPath)
+const whatsappSVG = readFileSync(whatsappSVGPath)
+
+const server = http.createServer((req, res) => {
+  
+  switch(req.url){
+    
+    ////main-files endpoints
+    case "/":
+      res.writeHead(200, {"Content-Type": "text/html"})
+      res.end(indexHTML)
+      break
+    case "/style.css":
+      res.writeHead(200, {"Content-Type": "text/css"})
+      res.end(cssFile)
+      break
+    case "/main.js": 
+      res.writeHead(200, {"Content-Type": "text/javascript"})
+      res.end(mainScript)
+      break
+    
+
+    ////images endpoints
+    
+    //header
+    case "/public/images/header-background.png":
+      res.writeHead(200, {"Content-Type": "image/png"})
+      res.end(headerBackground)
+      break
+
+    //welcome photos
+    case "/public/images/first-profile-photo.jpg":
+      res.writeHead(200, {"Content-Type": "image/jpg"})
+      res.end(welcomePhoto1)
+      break
+    case "/public/images/second-profile-photo.jpg":
+      res.writeHead(200, {"Content-Type": "image/jpg"})
+      res.end(welcomePhoto2)
+      break
+
+    case "/public/images/svg/stylized-svg/arrow-up.svg":
+      res.writeHead(200, {"Content-Type": "image/svg+xml"})
+      res.end(upArrow)
+      break
+      
+    //skill pictures
+    case "/public/images/svg/stylized-svg/web-programming.svg":
+      res.writeHead(200, {"Content-Type": "image/svg+xml"})
+      res.end(webDevSVG)
+      break
+    case "/public/images/svg/stylized-svg/statistics.svg":
+      res.writeHead(200, {"Content-Type": "image/svg+xml"})
+      res.end(mathAndStatsSVG)
+      break
+    case "/public/images/svg/stylized-svg/scatterplot.svg":
+      res.writeHead(200, {"Content-Type": "image/svg+xml"})
+      res.end(dataAnalysisSVG)
+      break
+    case "/public/images/svg/stylized-svg/robot.svg":
+      res.writeHead(200, {"Content-Type": "image/svg+xml"})
+      res.end(automationSVG)
+      break
+    
+    // portfolio arrows
+    case "/public/images/svg/stylized-svg/arrow-left.svg":
+      res.writeHead(200, {"Content-Type": "image/svg+xml"})
+      res.end(leftArrow)
+      break
+    case "/public/images/svg/stylized-svg/arrow-right.svg":
+      res.writeHead(200, {"Content-Type": "image/svg+xml"})
+      res.end(rightArrow)
+      break
+
+    //project backgrounds
+    case "/public/images/calculator-background.jpg":
+      res.writeHead(200, {"Content-Type": "image/jpg"})
+      res.end(calculatorProjectBackground)
+      break
+    case "/public/images/rgb-filter-background.jpg":
+      res.writeHead(200, {"Content-Type": "image/jpg"})
+      res.end(rgbFilterProjectBackground)
+      break
+
+    //svg layers
+    case "/public/images/svg/layers/layer1.svg":
+      res.writeHead(200, {"Content-Type": "image/svg+xml"})
+      res.end(layer1)
+      break
+    case "/public/images/svg/layers/layer2.svg":
+      res.writeHead(200, {"Content-Type": "image/svg+xml"})
+      res.end(layer2)
+      break
+
+    //social-media logos
+    case "/public/images/svg/social-media-logos/Golden-SVG-logos/1-Golden-github-logo.svg":
+      res.writeHead(200, {"Content-Type": "image/svg+xml"})
+      res.end(gitHubSVG)
+      break
+    case "/public/images/svg/social-media-logos/Golden-SVG-logos/2-Golden-linkedIn-logo.svg":
+      res.writeHead(200, {"Content-Type": "image/svg+xml"})
+      res.end(linkedInSVG)
+      break
+    case "/public/images/svg/social-media-logos/Golden-SVG-logos/3-Golden-Instagram-logo.svg":
+      res.writeHead(200, {"Content-Type": "image/svg+xml"})
+      res.end(instagramSVG)
+      break
+    case "/public/images/svg/social-media-logos/Golden-SVG-logos/4-Golden-e-mail-logo.svg":
+      res.writeHead(200, {"Content-Type": "image/svg+xml"})
+      res.end(emailSVG)
+      break
+    case "/public/images/svg/social-media-logos/Golden-SVG-logos/5-Golden-whatsapp-logo.svg":
+      res.writeHead(200, {"Content-Type": "image/svg+xml"})
+      res.end(whatsappSVG)
+      break
+
+    default:
+      res.statusCode = 404
+      res.end("There's been an error when trying to access the server!")
+  }
+}
+)
+
+const port = process.env.PORT || 8080
+
+server.listen(port, ()=>{
+  console.log(`Server is listening on port: ${port}`)
+})
---
+++ main.js
@@ -0,0 +1,388 @@
+// const paragraphs = document.querySelectorAll
+const header = document.querySelector("header")
+const article = document.querySelector("article")
+const titleChars = document.querySelectorAll(".title-char")
+const photos = document.querySelectorAll(".photo")
+const welcomeSection = document.querySelector("#welcome-section")
+const skillsSection = document.querySelector("#skills-section")
+const portfolioSection = document.querySelector("#portfolio-section")
+const aboutSection = document.querySelector("#about-section")
+
+
+const logos = document.querySelectorAll(".logo")
+const leftArrow = document.querySelector("#left-arrow")
+const rightArrow = document.querySelector("#right-arrow")
+const upArrow = document.querySelector(".up-arrow")
+const projects = document.querySelectorAll(".project")
+const skillCircles = document.querySelectorAll(".skill-circle")
+const calculatorProject = document.querySelector("#calculator-project")
+const rgbFilterProject = document.querySelector("#rgb-filter-project")
+const expertiseLink = document.querySelector("#expertise-link")
+const portfolioLink = document.querySelector("#portfolio-link")
+const aboutLink = document.querySelector("#about-link")
+
+
+////////////////////////////////////////
+//////// navbar links
+////////////////////////////////////////
+
+expertiseLink.addEventListener(
+  "click",
+  ()  => {
+    skillsSection.scrollIntoView( 
+      {behavior: "smooth"}
+    )
+    setTimeout(
+      () => {
+        skillsSection.scroll(
+          {
+            top: 50,
+            behavior: "smooth",
+          }
+        )
+      }, 1000)
+    setTimeout(
+      () => {
+        console.log("puta que pariu, porra!")
+      }, 1000)
+  }
+)
+portfolioLink.addEventListener(
+  "click",
+  ()  => {
+    portfolioSection.scrollIntoView(
+      {behavior: "smooth"}
+    )
+    setTimeout(
+      () => {
+        portfolioSection.scroll(
+          {
+            top: 50,
+            behavior: "smooth",
+          }
+        )
+      }, 400)
+  }
+)
+aboutLink.addEventListener(
+  "click",
+  ()  => {
+    aboutSection.scrollIntoView(
+      {behavior: "smooth"}
+    )
+    setTimeout(
+      () => {
+        aboutSection.scroll(
+          {
+            top: 50,
+            behavior: "smooth",
+          }
+        )
+      }, 400)
+  }
+)
+
+
+////////////////////////////////////////
+//////// CSS animations
+////////////////////////////////////////
+
+////// header title
+
+const startCascadeAnimation = () => {
+  titleChars.forEach(
+    (char) => {char.classList.add("running-animation")}
+  )
+  logos.forEach(
+    (logo) => {logo.classList.add("running-animation")}
+  )
+}
+
+window.addEventListener(
+  "DOMContentLoaded", 
+  () => setTimeout(startCascadeAnimation, 10000)
+)
+
+////// welcome photos
+
+const slidingKeyFrames = [
+  {
+    opacity: 1,
+    scale: 1,
+  },
+  {
+    opacity: 1,
+    scale: 1.05,
+  },
+  {
+    opacity: 0,
+    scale: 1.05,
+  },
+]
+
+let photoIndex = 0
+
+const slidingKeyFramesDuration = 8000
+
+const slidingKeyFramesOptions = {
+  duration: slidingKeyFramesDuration,
+  iterations: 1,
+  offset: [0, 0.99, 1],
+}
+
+const slidingPhotos = () => {
+
+  console.log("sliding function")
+
+  //removing all photos that differ from index-referenced photo 
+    //to avoid unintentional overlapping
+  for(let i = 0; i < photos.length; i++) {
+    photos[i].addEventListener(
+      "animationend", 
+      (event) => {
+        console.log(event)
+        slidingPhotos()
+      }
+    )
+
+    if(photos[i].classList.contains("top-welcome-photo")){
+      photos[i].classList.remove("top-welcome-photo")
+    } else if(i != photoIndex.valueOf()) {
+      welcomeSection.removeChild(photos[i])
+    }
+  }
+  
+  //animating index-referenced photo
+  photos[photoIndex].classList.add("top-welcome-photo")
+  photos[photoIndex].animate(slidingKeyFrames, slidingKeyFramesOptions)
+
+  //setting next value for photoIndex
+  photoIndex < (photos.length - 1) ? photoIndex =+ 1 : photoIndex = 0 
+
+  //appending next index-referenced photo
+    //before the animation is over
+  setTimeout(
+    () => {
+      welcomeSection.appendChild(photos[photoIndex])
+      console.log("new photo's been appended!")
+    }, (slidingKeyFramesDuration/2)
+  )
+}
+
+//setting listeners that will call the function
+
+// const attachAnimationEndListeners = () => {
+//   photos.forEach(
+//     (photo) => {
+//       photo.addEventListener(
+//         "animationend",
+//         (event) => {
+//           console.log("an animation has ended!")
+//           console.log(event)
+//           slidingPhotos()
+//         }
+//       )
+//     }
+//   )
+// }
+
+// window.addEventListener(
+//   "DOMContentLoaded",
+//   () => {
+//     slidingPhotos()
+//   }
+// )
+
+// setInterval(
+//   () => {
+//     slidingPhotos()
+//   },
+//   slidingKeyFramesDuration
+// )
+
+// window.addEventListener('DOMContentLoaded', () => {
+//   setInterval(slidingAnimation, 4000);
+// });
+
+// let photoIndex = 0
+
+// const slidingAnimation = () => {  
+  
+//   photos.forEach(
+//     (photo) => {
+//       if(photo.classList.contains("sliding-photo")) {
+//         photo.classList.remove("sliding-photo")
+//       } else if (welcomeSection.contains(photo)) {
+//         welcomeSection.removeChild(photo)
+//       }
+//     }
+//   )
+
+//   welcomeSection.appendChild(photos[photoIndex])
+//   photos[photoIndex].classList.add("sliding-photo")
+  
+//   if(photoIndex === 0) {
+//     welcomeSection.appendChild(photos[photoIndex + 1])
+//   } else {
+//     welcomeSection.appendChild(photos[photoIndex - 1])
+//   } 
+  
+//   photoIndex === 0 ? photoIndex = 1 : photoIndex = 0
+// }
+
+// slidingAnimation()
+
+// const slidingAnimation = () => {  
+  
+//   photos.forEach(photo => photo.classList.remove("sliding-photo"))
+//   photos[photoIndex].classList.add("sliding-photo")
+
+//   photoIndex += 1
+//   photoIndex === photos.length ? photoIndex = 0 : null
+// }
+
+// slidingAnimation()
+
+////////////////////////////////////////
+//////// scrolling effects
+////////////////////////////////////////
+
+//// growing skill circles
+
+const createSkillCircleObserver = () => {
+  const callback = (entries, skillCircleObserver) => {
+    entries.forEach(
+      (entry) => {
+        if(entry.isIntersecting) {
+          entry.target.classList.add("rescale-skill-circle")
+        } 
+      }
+    )
+  }
+  const skillCircleObserver = new IntersectionObserver(callback)
+  skillCircles.forEach(
+    (skillCircle) => {
+      skillCircleObserver.observe(skillCircle)
+    }
+  )
+}
+
+window.addEventListener(
+  "DOMContentLoaded", 
+  createSkillCircleObserver
+)
+
+//// fade-in and -out
+
+const aboutSectionObserver = () => {
+  
+  const aboutSectionObserverCallback = (entries, observer) => {
+    entries.forEach((entry) => {
+      if(entry.isIntersecting) {
+        entry.target.classList.add("vai-pra-pqp")
+      }
+    })
+  }
+
+  const mainObserver = new IntersectionObserver(mainObserverCallback, options)
+  paragraphs.forEach(element => mainObserver.observe(element))
+}
+
+
+//// after first scroll
+
+const shrinkHeaderAndShowArrow = () => {
+  const scrollPosition = window.scrollY
+
+  const shrinkThreshold = 100
+
+  if(scrollPosition > shrinkThreshold) {
+    header.classList.add("shrunk-header")
+    upArrow.classList.add("visible-up-arrow")
+  } else {
+    header.classList.remove("shrunk-header")
+    upArrow.classList.remove("visible-up-arrow")
+  }
+}
+
+window.addEventListener(
+  "scroll", 
+  shrinkHeaderAndShowArrow
+)
+
+const upArrowFunction = () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  })
+}
+
+upArrow.addEventListener(
+  "click",
+  upArrowFunction
+)
+
+///////////////////////////////////////
+//////////////Portfolio effect
+//////////////////////////////////////
+
+const ArrowFunction = () => {
+  
+  for(let i = 0; i < projects.length; i++){
+    if(projects[i].classList.contains("highlighted-project")){
+      projects[i].style.transform = "translateX(-200%)"
+      projects[i].style.display = "none"
+      projects[i].classList.replace("highlighted-project", "hidden-project")
+      console.log("i've removed the highlighted project")
+      console.log(projects[i])
+      continue
+    } if(projects[i].classList.contains("hidden-project")){
+      projects[i].style.transform = "translateX(200%)"
+      projects[i].style.display = "initial"        
+      projects[i].style.transform = "translateX(0%)"
+      projects[i].classList.replace("hidden-project", "highlighted-project")
+      console.log(projects[i])
+    }
+  }
+}
+
+leftArrow.addEventListener(
+  "click", 
+  ArrowFunction
+)
+rightArrow.addEventListener(
+  "click", 
+  ArrowFunction
+)
+
+// const linkToProject = (stringURL) => {
+//   const tempLink = document.createElement("a")
+//   tempLink.setAttribute("href", stringURL)
+//   document.body.appendChild(tempLink)
+//   tempLink.click()
+//   document.body.removeChild(tempLink)
+// }
+
+rgbFilterProject.addEventListener(
+  "click",
+  () => {
+    const tempLink = document.createElement("a")
+    tempLink.setAttribute("href", "https://image-modifier-app.uc.r.appspot.com/")
+    tempLink.setAttribute("target", "_blank");
+    document.body.appendChild(tempLink)
+    tempLink.click()
+    document.body.removeChild(tempLink)
+  }  
+)
+
+calculatorProject.addEventListener(
+  "click",
+  () => {
+    const tempLink = document.createElement("a")
+    tempLink.setAttribute("href", "https://tomazlanza.github.io/calculator/")
+    tempLink.setAttribute("target", "_blank");
+    document.body.appendChild(tempLink)
+    tempLink.click()
+    document.body.removeChild(tempLink)
+  }  
+)
